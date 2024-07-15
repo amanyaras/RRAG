2ac0634387e1c7740179e773d8cd712fa4b4ba0e
amanyaras <2539110495@qq.com>
2024-07-15 15:48:44 +0800

    add some files

---
+++ data/mapping.py
@@ -0,0 +1,10 @@
+import os
+
+
+DATA_PATH = [
+    "/home/zhangyh/datasets/wikiQA_gpt",
+    "/home/zhangyh/datasets/squad/plain_text",
+    "/home/zhangyh/datasets/hotpot_qa",
+    "/home/zhangyh/datasets/adversarial_qa",
+    "/home/zhangyh/datasets/ms_marco",
+]
